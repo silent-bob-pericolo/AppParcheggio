68809191738db96128733060dec8d06997b2b595
alessandro <m.cherubini52@gmail.com>
2021-03-11 11:49:33 +0100

    Aggiungere i file di progetto.

---
+++ ParchTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parchimetro;
+
+namespace ParchTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
